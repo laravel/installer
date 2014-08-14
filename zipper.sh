67d23b8ed1d8c7404c24949c4a354c068f13b96e
Taylor Otwell <taylorotwell@gmail.com>
2014-08-14 10:47:43 -0500

    update a few things.

---
+++ zipper.sh
@@ -0,0 +1,9 @@
+wget https://github.com/laravel/laravel/archive/master.zip
+unzip master.zip -d working
+cd working/laravel-master
+composer install
+zip -r ../../laravel-craft.zip *
+cd ../..
+mv laravel-craft.zip public/laravel-craft.zip
+rm -rf working
+rm master.zip
