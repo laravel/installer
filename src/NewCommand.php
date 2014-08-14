a758e59116302ebe18a6945a42f91664823e5d49
Taylor Otwell <taylorotwell@gmail.com>
2014-08-14 09:57:51 -0500

    new composer based installer.

---
+++ src/NewCommand.php
@@ -0,0 +1,121 @@
+<?php namespace Laravel\Installer\Console;
+
+use ZipArchive;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NewCommand extends \Symfony\Component\Console\Command\Command {
+
+	/**
+	 * Configure the command options.
+	 *
+	 * @return void
+	 */
+	protected function configure()
+	{
+		$this->setName('new')
+				->setDescription('Create a new Laravel application.')
+				->addArgument('name', InputArgument::REQUIRED);
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @param  InputInterface  $input
+	 * @param  OutputInterface  $output
+	 * @return void
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$this->verifyApplicationDoesntExist(
+			$directory = getcwd().'/'.$input->getArgument('name')
+		);
+
+		$output->writeln('<info>Crafting application...</info>');
+
+		$this->download($zipFile = $this->makeFilename())
+             ->extract($zipFile, $directory)
+             ->cleanUp($zipFile);
+
+		$output->writeln('<comment>Application ready! Build something amazing.</comment>');
+	}
+
+	/**
+	 * Verify that the application does not already exist.
+	 *
+	 * @param  string  $directory
+	 * @return void
+	 */
+	protected function verifyApplicationDoesntExist($directory)
+	{
+		if (is_dir($directory))
+		{
+			$output->writeln('<error>Application already exists!</error>');
+
+			exit(1);
+		}
+	}
+
+	/**
+	 * Generate a random temporary filename.
+	 *
+	 * @return string
+	 */
+	protected function makeFilename()
+	{
+		return getcwd().'/laravel_'.md5(time().uniqid()).'.zip';
+	}
+
+	/**
+	 * Download the temporary Zip to the given file.
+	 *
+	 * @param  string  $zipFile
+	 * @return $this
+	 */
+	protected function download($zipFile)
+	{
+		$response = \GuzzleHttp\get('http://192.241.224.13/laravel-craft.zip')->getBody();
+
+		file_put_contents($zipFile, $response);
+
+		return $this;
+	}
+
+	/**
+	 * Extract the zip file into the given directory.
+	 *
+	 * @param  string  $zipFile
+	 * @param  string  $directory
+	 * @return $this
+	 */
+	protected function extract($zipFile, $directory)
+	{
+		$archive = new ZipArchive;
+
+		$archive->open($zipFile);
+
+		$archive->extractTo($directory);
+
+		$archive->close();
+
+		return $this;
+	}
+
+	/**
+	 * Clean-up the Zip file.
+	 *
+	 * @param  string  $zipFile
+	 * @return $this
+	 */
+	protected function cleanUp($zipFile)
+	{
+		@chmod($zipFile, 0777);
+
+		@unlink($zipFile);
+
+		return $this;
+	}
+
+}
