11262c12ef84854d40dc274bb4405a798320a71a
Thomas Ruiz <contact@thomasruiz.eu>
2014-12-08 19:03:46 +0000

    follow psr1/2

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -1,122 +1,121 @@
 <?php namespace Laravel\Installer\Console;
 
-use ZipArchive;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use ZipArchive;
 
-class NewCommand extends \Symfony\Component\Console\Command\Command {
+class NewCommand extends \Symfony\Component\Console\Command\Command
+{
 
-	/**
-	 * Configure the command options.
-	 *
-	 * @return void
-	 */
-	protected function configure()
-	{
-		$this->setName('new')
-				->setDescription('Create a new Laravel application.')
-				->addArgument('name', InputArgument::REQUIRED);
-	}
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName('new')
+            ->setDescription('Create a new Laravel application.')
+            ->addArgument('name', InputArgument::REQUIRED);
+    }
 
-	/**
-	 * Execute the command.
-	 *
-	 * @param  InputInterface  $input
-	 * @param  OutputInterface  $output
-	 * @return void
-	 */
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		$this->verifyApplicationDoesntExist(
-			$directory = getcwd().'/'.$input->getArgument('name'),
-			$output
-		);
+    /**
+     * Execute the command.
+     *
+     * @param  InputInterface $input
+     * @param  OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->verifyApplicationDoesntExist(
+            $directory = getcwd() . '/' . $input->getArgument('name'),
+            $output
+        );
 
-		$output->writeln('<info>Crafting application...</info>');
+        $output->writeln('<info>Crafting application...</info>');
 
-		$this->download($zipFile = $this->makeFilename())
-             ->extract($zipFile, $directory)
-             ->cleanUp($zipFile);
+        $this->download($zipFile = $this->makeFilename())
+            ->extract($zipFile, $directory)
+            ->cleanUp($zipFile);
 
-		$output->writeln('<comment>Application ready! Build something amazing.</comment>');
-	}
+        $output->writeln('<comment>Application ready! Build something amazing.</comment>');
+    }
 
-	/**
-	 * Verify that the application does not already exist.
-	 *
-	 * @param  string  $directory
-	 * @return void
-	 */
-	protected function verifyApplicationDoesntExist($directory, OutputInterface $output)
-	{
-		if (is_dir($directory))
-		{
-			$output->writeln('<error>Application already exists!</error>');
+    /**
+     * Verify that the application does not already exist.
+     *
+     * @param  string $directory
+     * @return void
+     */
+    protected function verifyApplicationDoesntExist($directory, OutputInterface $output)
+    {
+        if (is_dir($directory)) {
+            $output->writeln('<error>Application already exists!</error>');
 
-			exit(1);
-		}
-	}
+            exit(1);
+        }
+    }
 
-	/**
-	 * Generate a random temporary filename.
-	 *
-	 * @return string
-	 */
-	protected function makeFilename()
-	{
-		return getcwd().'/laravel_'.md5(time().uniqid()).'.zip';
-	}
+    /**
+     * Generate a random temporary filename.
+     *
+     * @return string
+     */
+    protected function makeFilename()
+    {
+        return getcwd() . '/laravel_' . md5(time() . uniqid()) . '.zip';
+    }
 
-	/**
-	 * Download the temporary Zip to the given file.
-	 *
-	 * @param  string  $zipFile
-	 * @return $this
-	 */
-	protected function download($zipFile)
-	{
-		$response = \GuzzleHttp\get('http://cabinet.laravel.com/latest.zip')->getBody();
+    /**
+     * Download the temporary Zip to the given file.
+     *
+     * @param  string $zipFile
+     * @return $this
+     */
+    protected function download($zipFile)
+    {
+        $response = \GuzzleHttp\get('http://cabinet.laravel.com/latest.zip')->getBody();
 
-		file_put_contents($zipFile, $response);
+        file_put_contents($zipFile, $response);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Extract the zip file into the given directory.
-	 *
-	 * @param  string  $zipFile
-	 * @param  string  $directory
-	 * @return $this
-	 */
-	protected function extract($zipFile, $directory)
-	{
-		$archive = new ZipArchive;
+    /**
+     * Extract the zip file into the given directory.
+     *
+     * @param  string $zipFile
+     * @param  string $directory
+     * @return $this
+     */
+    protected function extract($zipFile, $directory)
+    {
+        $archive = new ZipArchive;
 
-		$archive->open($zipFile);
+        $archive->open($zipFile);
 
-		$archive->extractTo($directory);
+        $archive->extractTo($directory);
 
-		$archive->close();
+        $archive->close();
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Clean-up the Zip file.
-	 *
-	 * @param  string  $zipFile
-	 * @return $this
-	 */
-	protected function cleanUp($zipFile)
-	{
-		@chmod($zipFile, 0777);
+    /**
+     * Clean-up the Zip file.
+     *
+     * @param  string $zipFile
+     * @return $this
+     */
+    protected function cleanUp($zipFile)
+    {
+        @chmod($zipFile, 0777);
 
-		@unlink($zipFile);
+        @unlink($zipFile);
 
-		return $this;
-	}
+        return $this;
+    }
 
 }
