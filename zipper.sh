0738d3168581a345e97b29f1070fe06d6b3ad426
RiDdLeS <riddles@dev-toolbox.com>
2015-03-03 01:20:40 -0600

    updating composer lock and removing zipper

--- zipper.sh
+++
@@ -1,9 +0,0 @@
-wget https://github.com/laravel/laravel/archive/master.zip
-unzip master.zip -d working
-cd working/laravel-master
-composer install
-zip -r ../../laravel-craft.zip .
-cd ../..
-mv laravel-craft.zip public/laravel-craft.zip
-rm -rf working
-rm master.zip
