896d9a749aeda4a4aa4377fdc0d0675465a9b19c
Arjan Schouten <arjan-schouten@live.nl>
2015-07-03 13:03:55 +0200

    Added option to install Homestead per project along with installer

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -2,11 +2,14 @@
 
 namespace Laravel\Installer\Console;
 
+use finfo;
 use ZipArchive;
 use RuntimeException;
 use GuzzleHttp\Client;
+use Illuminate\Support\Str;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,7 +25,8 @@ class NewCommand extends Command
     {
         $this->setName('new')
              ->setDescription('Create a new Laravel application.')
-             ->addArgument('name', InputArgument::REQUIRED);
+             ->addArgument('name', InputArgument::REQUIRED)
+             ->addOption('with-homestead', null, InputOption::VALUE_NONE, 'Create a new Laravel application with Laravel Homestead included.');
     }
 
     /**
@@ -58,6 +62,10 @@ class NewCommand extends Command
         $process->run(function ($type, $line) use ($output) {
             $output->write($line);
         });
+
+        if ($input->getOption('with-homestead')) {
+            $this->includeHomestead($directory, $output);
+        }
 
         $output->writeln('<comment>Application ready! Build something amazing.</comment>');
     }
@@ -147,5 +155,81 @@ class NewCommand extends Command
         }
 
         return 'composer';
+    }
+
+    /**
+     * Include Laravel Homestead in the new application.
+     *
+     * @param  string  $directory
+     * @param  OutputInterface  $output
+     * @return void
+     */
+    protected function includeHomestead($directory, OutputInterface $output)
+    {
+        $output->writeln('<info>Installing Laravel Homestead...</info>');
+
+        $composer = $this->findComposer();
+
+        $process = new Process($composer.' require laravel/homestead', $directory, null, null, null);
+
+        $process->run();
+
+        $initScriptPath = 'vendor/bin/homestead';
+
+        // Composer can create binary proxy files in the bin directory of the vendor
+        // directory. On Windows machines composer creates .bat proxy files.
+        if ($this->isPHPScript($directory.'/'.$initScriptPath)) {
+            $makeCommand = '"'.PHP_BINARY.'" '.$initScriptPath;
+        } elseif ($this->isWindows()) {
+            $makeCommand = 'call '.$initScriptPath.'.bat make';
+        } else {
+            $makeCommand = $initScriptPath.' make';
+        }
+
+        $process = new Process($makeCommand, $directory, null, null, null);
+
+        try {
+            $process->run();
+        } catch (RuntimeException $e) {
+            $output->writeln('<error>'.$e->getMessage().'</error>');
+            $output->writeln('<error>Skipping further installation of Laravel Homestead.</error>');
+            return;
+        }
+
+        $output->writeln('<comment>Laravel Homestead installed successfully.</comment>');
+    }
+
+    /**
+     * Check if a script is a PHP script.
+     *
+     * @param  string  $file
+     * @return bool
+     * @throws RuntimeException
+     */
+    protected function isPHPScript($file)
+    {
+        // As of PHP 5.3 the fileinfo extension is enabled by default on unix machines.
+        // On Windows the fileinfo extension is disabled by default in php.ini.
+        if (!extension_loaded('fileinfo')) {
+            throw new RuntimeException('Fileinfo extension must be enabled to install Laravel Homestead.');
+        }
+
+        $file_info = new finfo(FILEINFO_MIME);
+
+        $file_info->finfo();
+
+        $mime_type = $file_info->buffer(file_get_contents($file));
+
+        return Str::contains($mime_type, 'php');
+    }
+
+    /**
+     * Check if the current OS is Windows.
+     *
+     * @return bool
+     */
+    protected function isWindows()
+    {
+        return strpos(strtoupper(PHP_OS), 'WIN') === 0;
     }
 }
