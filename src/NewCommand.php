3044997716d6e4fddac315f5d26689382bb378bc
Mohamed Said <themohamedsaid@gmail.com>
2017-08-30 12:09:11 +0200

    remove command

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -64,7 +64,6 @@ class NewCommand extends Command
         $commands = [
             $composer.' install --no-scripts',
             $composer.' run-script post-root-package-install',
-            $composer.' run-script post-install-cmd',
             $composer.' run-script post-create-project-cmd',
         ];
 
