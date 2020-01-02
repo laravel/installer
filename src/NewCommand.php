d6587bbb70b5cb5bf143e4d52cab281b5a9cd879
Regis Zanandrea <regiszanandrea@gmail.com>
2020-01-02 15:10:07 -0300

    Remove blank line

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -39,8 +39,7 @@ class NewCommand extends Command
         'dusk' => 'dusk:install',
         'scout' => 'vendor:publish --provider="Laravel\Scout\ScoutServiceProvider"',
     ];
-
-
+    
     /**
      * Configure the command options.
      *
