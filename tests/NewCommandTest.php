e325afdb122316d1549b4c89f69a123ecfdab92e
Regis Zanandrea <regiszanandrea@gmail.com>
2020-01-02 14:05:42 -0300

    Add test for with option

--- tests/NewCommandTest.php
+++ tests/NewCommandTest.php
@@ -13,7 +13,7 @@ class NewCommandTest extends TestCase
     public function test_it_can_scaffold_a_new_laravel_app()
     {
         $scaffoldDirectoryName = 'tests-output/my-app';
-        $scaffoldDirectory = __DIR__.'/../'.$scaffoldDirectoryName;
+        $scaffoldDirectory = __DIR__ . '/../' . $scaffoldDirectoryName;
 
         if (file_exists($scaffoldDirectory)) {
             (new Filesystem)->remove($scaffoldDirectory);
@@ -27,8 +27,29 @@ class NewCommandTest extends TestCase
         $statusCode = $tester->execute(['name' => $scaffoldDirectoryName, '--auth' => null]);
 
         $this->assertEquals($statusCode, 0);
-        $this->assertDirectoryExists($scaffoldDirectory.'/vendor');
-        $this->assertFileExists($scaffoldDirectory.'/.env');
-        $this->assertFileExists($scaffoldDirectory.'/resources/views/auth/login.blade.php');
+        $this->assertDirectoryExists($scaffoldDirectory . '/vendor');
+        $this->assertFileExists($scaffoldDirectory . '/.env');
+        $this->assertFileExists($scaffoldDirectory . '/resources/views/auth/login.blade.php');
+    }
+
+    public function test_it_can_scaffold_a_new_laravel_app_with_packages()
+    {
+        $scaffoldDirectoryName = 'tests-output/my-app';
+        $scaffoldDirectory = __DIR__ . '/../' . $scaffoldDirectoryName;
+
+        if (file_exists($scaffoldDirectory)) {
+            (new Filesystem)->remove($scaffoldDirectory);
+        }
+
+        $app = new Application('Laravel Installer');
+        $app->add(new NewCommand);
+
+        $tester = new CommandTester($app->find('new'));
+
+        $statusCode = $tester->execute(['name' => $scaffoldDirectoryName, '--auth' => null, '--with' => 'telescope,horizon']);
+
+        $this->assertEquals($statusCode, 0);
+        $this->assertFileExists($scaffoldDirectory . '/config/telescope.php');
+        $this->assertFileExists($scaffoldDirectory . '/config/horizon.php');
     }
 }
