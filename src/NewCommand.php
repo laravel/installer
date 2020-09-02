dcdb13a814e97fffeb84b6b8a123717bc27f55b6
Taylor Otwell <taylorotwell@users.noreply.github.com>
2020-09-02 19:59:40 +0000

    Apply fixes from StyleCI

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -9,7 +9,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process;
 
