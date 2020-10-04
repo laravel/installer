100d72d5bae63b0ad70e1deb3c3abc924c107dfa
Oliver Matla <info@lupinitylabs.com>
2020-10-04 16:49:44 +0200

    Add a test case for assuring that installation in the current working directory succeeds

--- tests/NewCommandTest.php
+++ tests/NewCommandTest.php
@@ -12,15 +12,7 @@ class NewCommandTest extends TestCase
     public function test_it_can_scaffold_a_new_laravel_app()
     {
         $scaffoldDirectoryName = 'tests-output/my-app';
-        $scaffoldDirectory = __DIR__.'/../'.$scaffoldDirectoryName;
-
-        if (file_exists($scaffoldDirectory)) {
-            if (PHP_OS_FAMILY == 'Windows') {
-                exec("rd /s /q \"$scaffoldDirectory\"");
-            } else {
-                exec("rm -rf \"$scaffoldDirectory\"");
-            }
-        }
+        $scaffoldDirectory = $this->prepareScaffoldDirectory($scaffoldDirectoryName);
 
         $app = new Application('Laravel Installer');
         $app->add(new NewCommand);
@@ -32,5 +24,41 @@ class NewCommandTest extends TestCase
         $this->assertSame(0, $statusCode);
         $this->assertDirectoryExists($scaffoldDirectory.'/vendor');
         $this->assertFileExists($scaffoldDirectory.'/.env');
+    }
+
+    public function test_it_can_scaffold_a_new_laravel_app_in_the_current_directory()
+    {
+        $scaffoldDirectoryName = 'tests-output/my-app';
+        $scaffoldDirectory = $this->prepareScaffoldDirectory($scaffoldDirectoryName);
+
+        // Create directory and change into it.
+        mkdir($scaffoldDirectory);
+        chdir($scaffoldDirectory);
+
+        $app = new Application('Laravel Installer');
+        $app->add(new NewCommand);
+
+        $tester = new CommandTester($app->find('new'));
+
+        $statusCode = $tester->execute([]);
+
+        $this->assertSame(0, $statusCode);
+        $this->assertDirectoryExists($scaffoldDirectory.'/vendor');
+        $this->assertFileExists($scaffoldDirectory.'/.env');
+    }
+
+    public function prepareScaffoldDirectory($scaffoldDirectoryName)
+    {
+        $scaffoldDirectory = __DIR__.'/../'.$scaffoldDirectoryName;
+
+        if (file_exists($scaffoldDirectory)) {
+            if (PHP_OS_FAMILY == 'Windows') {
+                exec("rd /s /q \"$scaffoldDirectory\"");
+            } else {
+                exec("rm -rf \"$scaffoldDirectory\"");
+            }
+        }
+
+        return $scaffoldDirectory;
     }
 }
