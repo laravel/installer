6ba41808c3e9c27c15d0e72502d27cc8cf3ab6e1
Taylor Otwell <taylor@laravel.com>
2021-03-18 10:26:09 -0500

    Update NewCommand.php

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -28,7 +28,7 @@ class NewCommand extends Command
             ->addOption('dev', null, InputOption::VALUE_NONE, 'Installs the latest "development" release')
             ->addOption('git', null, InputOption::VALUE_NONE, 'Initialize a Git repository')
             ->addOption('github', null, InputOption::VALUE_OPTIONAL, 'Create a new repository on GitHub', false)
-            ->addOption('organisation', null, InputOption::VALUE_REQUIRED, 'Choose the GitHub organisation to create the new repository for.')
+            ->addOption('organization', null, InputOption::VALUE_REQUIRED, 'The GitHub organization to create the new repository for')
             ->addOption('jet', null, InputOption::VALUE_NONE, 'Installs the Laravel Jetstream scaffolding')
             ->addOption('stack', null, InputOption::VALUE_OPTIONAL, 'The Jetstream stack that should be installed')
             ->addOption('teams', null, InputOption::VALUE_NONE, 'Indicates whether Jetstream should be scaffolded with team support')
@@ -263,7 +263,8 @@ class NewCommand extends Command
 
         chdir($directory);
 
-        $name = $input->getOption('organisation') ? $input->getOption('organisation')."/$name" : $name;
+        $name = $input->getOption('organization') ? $input->getOption('organization')."/$name" : $name;
+
         $flags = $input->getOption('github') ?: '--private';
 
         $commands = [
