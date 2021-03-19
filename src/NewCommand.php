4816a034e5048164789f994a44368f7c2d8d8736
Dries Vints <dries@vints.io>
2021-03-19 15:52:22 +0100

    Update pushing branch (#196)

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -269,10 +269,10 @@ class NewCommand extends Command
 
         $commands = [
             "gh repo create $name -y $flags",
-            'git push -q -u origin main',
+            "git -c credential.helper= -c credential.helper='!gh auth git-credential' push -q -u origin main",
         ];
 
-        $this->runCommands($commands, $input, $output);
+        $this->runCommands($commands, $input, $output, ['GIT_TERMINAL_PROMPT' => 0]);
     }
 
     /**
@@ -325,9 +325,10 @@ class NewCommand extends Command
      * @param  array  $commands
      * @param  \Symfony\Component\Console\Input\InputInterface  $input
      * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  array  $env
      * @return \Symfony\Component\Process\Process
      */
-    protected function runCommands($commands, InputInterface $input, OutputInterface $output)
+    protected function runCommands($commands, InputInterface $input, OutputInterface $output, array $env = [])
     {
         if ($input->getOption('no-ansi')) {
             $commands = array_map(function ($value) {
@@ -349,7 +350,7 @@ class NewCommand extends Command
             }, $commands);
         }
 
-        $process = Process::fromShellCommandline(implode(' && ', $commands), null, null, null, null);
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, $env, null, null);
 
         if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
             try {
