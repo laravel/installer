055b25633cc0b4db438bf70c870f67766b6fdf0d
Dries Vints <dries@vints.io>
2022-06-28 17:57:56 +0200

    Update NewCommand.php (#232)

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -176,7 +176,7 @@ class NewCommand extends Command
         $commands = array_filter([
             $this->findComposer().' require laravel/jetstream',
             trim(sprintf(PHP_BINARY.' artisan jetstream:install %s %s', $stack, $teams ? '--teams' : '')),
-            'npm install && npm run dev',
+            'npm install && npm run build',
             PHP_BINARY.' artisan storage:link',
         ]);
 
