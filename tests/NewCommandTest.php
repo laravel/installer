c3131efbf3962ed77caf3da44e6ebad3a236b41c
Bilal Raja <rajabilal555@gmail.com>
2023-08-22 23:02:29 +0500

    updated scaffold test to use pest so the installPest function can be included in the test

--- tests/NewCommandTest.php
+++ tests/NewCommandTest.php
@@ -27,7 +27,7 @@ class NewCommandTest extends TestCase
 
         $tester = new CommandTester($app->find('new'));
 
-        $statusCode = $tester->execute(['name' => $scaffoldDirectoryName], ['interactive' => false]);
+        $statusCode = $tester->execute(['name' => $scaffoldDirectoryName, '--pest' => true], ['interactive' => false]);
 
         $this->assertSame(0, $statusCode);
         $this->assertDirectoryExists($scaffoldDirectory.'/vendor');
