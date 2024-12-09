5bb6708eac755c5bfa09b605553d0f42ed9b8938
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-09 12:47:03 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/NewCommandTest.php
+++ tests/NewCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Laravel\Installer\Console\Tests;
 
+use Laravel\Installer\Console\Concerns\InteractsWithHerdOrValet;
 use Laravel\Installer\Console\NewCommand;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Application;
@@ -11,6 +12,8 @@ use function Illuminate\Filesystem\join_paths;
 
 class NewCommandTest extends TestCase
 {
+    use InteractsWithHerdOrValet;
+
     public function test_it_can_scaffold_a_new_laravel_app()
     {
         $scaffoldDirectoryName = 'tests-output/my-app';
@@ -38,6 +41,10 @@ class NewCommandTest extends TestCase
 
     public function test_it_can_chops_trailing_slash_from_name()
     {
+        if ($this->runOnValetOrHerd('paths') === false) {
+            $this->markTestSkipped('Require `herd` or `valet` to resolve `APP_URL` using hostname instead of "localhost".');
+        }
+
         $scaffoldDirectoryName = 'tests-output/trailing/';
         $scaffoldDirectory = join_paths(__DIR__, '..', $scaffoldDirectoryName);
 
@@ -59,7 +66,13 @@ class NewCommandTest extends TestCase
         $this->assertSame(0, $statusCode);
         $this->assertDirectoryExists(join_paths($scaffoldDirectory, 'vendor'));
         $this->assertFileExists(join_paths($scaffoldDirectory, '.env'));
-        $this->assertStringContainsStringIgnoringLineEndings('APP_URL=http://tests-output/trailing.test', file_get_contents(join_paths($scaffoldDirectory, '.env')));
+
+        if ($this->isParkedOnHerdOrValet($scaffoldDirectory)) {
+            $this->assertStringContainsStringIgnoringLineEndings(
+                'APP_URL=http://tests-output/trailing.test', 
+                file_get_contents(join_paths($scaffoldDirectory, '.env'))
+            );
+        }
     }
 
     public function test_on_at_least_laravel_11()
