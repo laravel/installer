e24c5d83000940ed30150a6224a96efec2f56400
Tony Lea <tony@devdojo.com>
2025-02-12 14:53:46 -0500

    Adding more commands for PestPHP option with existing starter kits

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -529,6 +529,11 @@ class NewCommand extends Command
             $this->phpBinary().' ./vendor/bin/pest --init',
         ];
 
+        if ($input->getOption('react') || $input->getOption('vue') || $input->getOption('livewire')) {
+            $commands[] = $composerBinary.' require pestphp/pest-plugin-drift --dev';
+            $commands[] = $this->phpBinary().' ./vendor/bin/pest --drift';
+        }
+
         $this->runCommands($commands, $input, $output, workingPath: $directory, env: [
             'PEST_NO_SUPPORT' => 'true',
         ]);
@@ -542,6 +547,10 @@ class NewCommand extends Command
             'pest/Unit.php',
             $directory.'/tests/Unit/ExampleTest.php',
         );
+
+        if (($input->getOption('react') || $input->getOption('vue') || $input->getOption('livewire')) && $input->getOption('phpunit')) {
+            $this->deleteFile($directory.'/tests/Pest.php');
+        }
 
         $this->commitChanges('Install Pest', $directory, $input, $output);
     }
@@ -844,5 +853,16 @@ class NewCommand extends Command
             $file,
             preg_replace($pattern, $replace, file_get_contents($file))
         );
+    }
+
+    /**
+     * Delete the given file.
+     *
+     * @param  string  $file
+     * @return void
+     */
+    protected function deleteFile(string $file)
+    {
+        unlink($file);
     }
 }
