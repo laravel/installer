708c3f74ea17c75c913548088e05fee943171dc6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-04-20 18:52:46 -0500

    Extract createApplication helper in tests
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/NewCommandTest.php
+++ tests/NewCommandTest.php
@@ -25,12 +25,7 @@ class NewCommandTest extends TestCase
             }
         }
 
-        $app = new Application('Laravel Installer');
-        if (method_exists($app, 'addCommand')) {
-            $app->addCommand(new NewCommand);
-        } else {
-            $app->add(new NewCommand);
-        }
+        $app = $this->createApplication();
 
         $tester = new CommandTester($app->find('new'));
 
@@ -58,12 +53,7 @@ class NewCommandTest extends TestCase
             }
         }
 
-        $app = new Application('Laravel Installer');
-        if (method_exists($app, 'addCommand')) {
-            $app->addCommand(new NewCommand);
-        } else {
-            $app->add(new NewCommand);
-        }
+        $app = $this->createApplication();
 
         $tester = new CommandTester($app->find('new'));
 
@@ -115,5 +105,21 @@ class NewCommandTest extends TestCase
         $this->assertSame(getcwd().'/'.$relativePath, $command->getInstallationDirectoryPublic($relativePath));
 
         $this->assertSame('.', $command->getInstallationDirectoryPublic('.'));
+    }
+
+    /**
+     * @return \Symfony\Component\Console\Application
+     */
+    private function createApplication(): Application
+    {
+        $app = new Application('Laravel Installer');
+
+        if (method_exists($app, 'addCommand')) {
+            $app->addCommand(new NewCommand);
+        } else {
+            $app->add(new NewCommand);
+        }
+
+        return $app;
     }
 }
